b70b8eaf6223093b08d0c95f5e66e12982928194
Sean O'Connor <so@seano.dev>
2023-11-11 15:55:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UMCU API
+
+![Kotlin v1.8.22](https://img.shields.io/badge/Kotlin-v1.8.22-7a4ff3?logo=kotlin&logoColor=white) ![Spring v3.1.5](https://img.shields.io/badge/Spring-v3.1.5-6aad3d?logo=spring&logoColor=white)
---
+++ src/test/kotlin/app/umcu/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.umcu.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
