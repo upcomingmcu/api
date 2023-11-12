ce36cf8c793c479c80a3521ebb1496e85141504f
Sean O'Connor <so@seano.dev>
2023-11-12 11:48:07 -0700

    Create controller for `productions` endpoint

---
+++ src/main/kotlin/app/umcu/api/controllers/ProductionController.kt
@@ -0,0 +1,56 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <https://unlicense.org>
+ */
+
+@file:Suppress("unused")
+
+package app.umcu.api.controllers
+
+import app.umcu.api.models.APIResourceList
+import app.umcu.api.models.Production
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+@RestController
+@RequestMapping(path = ["/productions", "/p"])
+class ProductionController {
+
+	@GetMapping
+	fun getProductions(): APIResourceList<Production> {
+		return APIResourceList(*Production.sampleData)
+	}
+
+	@GetMapping("/{id}")
+	fun getProduction(@PathVariable id: Int): APIResourceList<Production> {
+		val production =
+			Production.sampleData.firstOrNull { it.id == id } ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+		return APIResourceList(production)
+	}
+}
