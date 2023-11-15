3c40bc4867fb0082f4eba90f7b424ba4792671e1
Sean O'Connor <so@seano.dev>
2023-11-15 16:50:01 -0700

    Define valid methods for productions controller

--- src/main/kotlin/app/umcu/api/controllers/ProductionsController.kt
+++ src/main/kotlin/app/umcu/api/controllers/ProductionsController.kt
@@ -33,15 +33,11 @@ import app.umcu.api.models.Production
 import app.umcu.api.repositories.ProductionsService
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
-@RequestMapping(path = ["/productions", "/p"])
+@RequestMapping(path = ["/productions", "/p"], method = [RequestMethod.GET])
 class ProductionsController(val productionsService: ProductionsService) {
 
 	@GetMapping
