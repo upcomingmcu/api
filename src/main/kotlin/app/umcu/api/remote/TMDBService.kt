53cf95af4f57e2436283699a0de5ceea1aabd77a
Sean O'Connor <so@seano.dev>
2023-11-21 15:59:54 -0700

    Update TMDBService
    Properly get and process all productions

--- src/main/kotlin/app/umcu/api/remote/TMDBService.kt
+++ src/main/kotlin/app/umcu/api/remote/TMDBService.kt
@@ -23,8 +23,9 @@ class TMDBService(
 ) : ApplicationRunner {
 	private val apiToken: String = System.getenv()["TMDB_READ_ACCESS_TOKEN"] ?: ""
 	private val logger: Logger = LoggerFactory.getLogger(TMDBService::class.java)
+	private val tmdbListId = 8254729 // TMDB list of all MCU productions
 
-	private inline fun <reified T : Any> get(@Suppress("SameParameterValue") url: String): T? {
+	private inline fun <reified T : Any> get(url: String): T? {
 		val headers = HttpHeaders()
 		headers.setBearerAuth(apiToken)
 		val response: ResponseEntity<T> = restTemplate.exchange(
@@ -35,44 +36,94 @@ class TMDBService(
 		return response.body
 	}
 
-	private fun getListDetails(@Suppress("SameParameterValue") listId: Int, page: Int = 1): TMDBListDetails? {
-		return get<TMDBListDetails>("https://api.themoviedb.org/3/list/$listId?page=$page")
+	private inline fun <reified T : Any> getTmdb(endpoint: String): T? {
+		val url = "https://api.themoviedb.org/3/$endpoint"
+		logger.info("GET $url")
+		return get<T>(url)
 	}
 
-	private fun getProductions(): ArrayList<Production> {
-		val listId = 8254729 // TMDB list of all MCU productions
-		val productions: ArrayList<Production> = ArrayList()
+	private fun parseDate(dateString: String?): LocalDate? {
+		return dateString?.takeIf { it.isNotEmpty() }?.let {
+			return LocalDate.parse(it, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+		}
+	}
+
+	private fun getListDetails(@Suppress("SameParameterValue") listId: Int, page: Int = 1): TMDBListDetails? {
+		return getTmdb<TMDBListDetails>("list/$listId?page=$page")
+	}
+
+	private fun getListItems(): ArrayList<TMDBListItem>? {
+		val listItems: ArrayList<TMDBListItem> = ArrayList()
 		var page = 1
 		do {
-			val tmdbListDetails = getListDetails(listId, page) ?: return ArrayList()
-			tmdbListDetails.items.forEach { item ->
-				if (item.mediaType == MediaType.Movie) {
-					val tmdbMovieDetails =
-						get<TMDBMovieDetails>("https://api.themoviedb.org/3/movie/${item.id}") ?: return ArrayList()
+			val tmdbListDetails = getListDetails(tmdbListId, page) ?: return null
+			listItems.addAll(tmdbListDetails.items)
+			page += 1
+		} while (tmdbListDetails.page != tmdbListDetails.totalPages)
+		return if (listItems.size <= 0) null else listItems
+	}
 
-					val production = Production(tmdbId = tmdbMovieDetails.id,
-						title = tmdbMovieDetails.title,
-						releaseDate = tmdbMovieDetails.releaseDate?.takeIf { it.isNotEmpty() }?.let {
-							LocalDate.parse(it, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-						}
+	private fun getMovieDetails(movieId: Int): TMDBMovieDetails? {
+		return getTmdb<TMDBMovieDetails>("movie/${movieId}")
+	}
 
+	private fun getSeriesDetails(seriesId: Int): TMDBTVSeriesDetails? {
+		return getTmdb<TMDBTVSeriesDetails>("tv/${seriesId}")
+	}
+
+	private fun handleMovies(listItems: Iterable<TMDBListItem>): ArrayList<Production> {
+		val productions: ArrayList<Production> = ArrayList()
+		listItems.filter { it.mediaType == MediaType.Movie }.forEach { item ->
+			val movieDetails = getMovieDetails(item.id)
+			if (movieDetails != null) {
+				val production = Production(
+					tmdbId = movieDetails.id,
+					title = movieDetails.title,
+					releaseDate = parseDate(movieDetails.releaseDate)
+				)
+				productions.add(production)
+			}
+		}
+		return productions
+	}
+
+	private fun handleSeries(listItems: Iterable<TMDBListItem>): ArrayList<Production> {
+		val productions: ArrayList<Production> = ArrayList()
+		listItems.filter { it.mediaType == MediaType.TV }.forEach { item ->
+			val seriesDetails = getSeriesDetails(item.id)
+			if (seriesDetails != null) {
+				if (seriesDetails.seasons.size > 1) {
+					seriesDetails.seasons.forEach { seasonDetails ->
+						val production = Production(
+							tmdbId = seriesDetails.id,
+							title = "${seriesDetails.name} Season ${seasonDetails.seasonNumber}",
+							releaseDate = parseDate(seasonDetails.airDate)
+						)
+						productions.add(production)
+					}
+				} else {
+					val season = seriesDetails.seasons.first()
+					val production = Production(
+						tmdbId = seriesDetails.id, title = seriesDetails.name, releaseDate = parseDate(season.airDate)
 					)
-					logger.info("Added production \"${production.slug}\"")
 					productions.add(production)
 				}
 			}
-			page += 1
-		} while (tmdbListDetails.page != tmdbListDetails.totalPages)
+		}
 		return productions
 	}
 
 	override fun run(args: ApplicationArguments?) {
-		/**
-		 * TODO
-		 * 	- Break down code into more functions
-		 * 	- Handle parsing TV productions (inc. multiple seasons)
-		 */
-		val productions = getProductions()
+		val listItems = getListItems() ?: return
+
+		val productions: ArrayList<Production> = ArrayList()
+		handleMovies(listItems).let {
+			productions.addAll(it)
+		}
+		handleSeries(listItems).let {
+			productions.addAll(it)
+		}
+
 		productionsRepository.saveAll(productions)
 	}
 }
