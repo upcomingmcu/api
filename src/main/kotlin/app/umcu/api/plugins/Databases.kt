49277f6bb3a6ef29cf4ebbee5d419200e7db4abe
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-17 18:25:38 -0700

    Configure Postgres database connection

--- src/main/kotlin/app/umcu/api/plugins/Databases.kt
+++ src/main/kotlin/app/umcu/api/plugins/Databases.kt
@@ -5,7 +5,7 @@ import java.sql.Connection
 import java.sql.DriverManager
 
 fun Application.configureDatabases() {
-	val dbConnection: Connection = connectToPostgres(embedded = true)
+	val dbConnection: Connection = connectToPostgres(embedded = false)
 }
 
 fun Application.connectToPostgres(embedded: Boolean): Connection {
@@ -13,9 +13,10 @@ fun Application.connectToPostgres(embedded: Boolean): Connection {
 	if (embedded) {
 		return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
 	} else {
-		val url = environment.config.property("postgres.url").getString()
+		val db = environment.config.property("postgres.db").getString()
 		val user = environment.config.property("postgres.user").getString()
 		val password = environment.config.property("postgres.password").getString()
+		val url = "jdbc:postgresql://db:5432/$db"
 		return DriverManager.getConnection(url, user, password)
 	}
 }
