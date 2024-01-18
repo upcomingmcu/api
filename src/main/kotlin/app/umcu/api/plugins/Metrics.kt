0e70b29ea59079314e218121b91ca1d63e31ca74
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-18 16:22:07 -0700

    Install MicrometerMetrics plugin

---
+++ src/main/kotlin/app/umcu/api/plugins/Metrics.kt
@@ -0,0 +1,31 @@
+package app.umcu.api.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+
+fun Application.configureMetrics() {
+	val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+	install(MicrometerMetrics) {
+		registry = appMicrometerRegistry
+		timers { call, _ ->
+			call.request.headers["regionId"]?.let { tag("region", it) }
+		}
+		meterBinders = listOf(
+			JvmMemoryMetrics(), JvmGcMetrics(), ProcessorMetrics()
+		)
+	}
+
+	routing {
+		get("/metrics") {
+			call.respond(appMicrometerRegistry.scrape())
+		}
+	}
+}
