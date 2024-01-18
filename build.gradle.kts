c9e2d7d840cc6b24335d87c1efdf133e8a2498e6
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-18 16:21:56 -0700

    Add Ktor metrics and Prometheus dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ kotlin {
 dependencies {
     implementation(libs.bundles.ktor.server)
     implementation(libs.ktor.serialization)
+    implementation(libs.micrometer.registry.prometheus)
     implementation(libs.logback)
     implementation(libs.h2)
     implementation(libs.postgresql)
