dacee86e54e4f3c824e4bcb11a425a38e2d480a2
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-22 10:30:12 -0700

    Update Production model
    Add `releaseDate` property, remove sample data

--- src/main/kotlin/app/umcu/api/models/Production.kt
+++ src/main/kotlin/app/umcu/api/models/Production.kt
@@ -1,16 +1,14 @@
 package app.umcu.api.models
 
 import app.umcu.api.utils.extensions.toSlug
+import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Production(
-	val slug: String, val tmdbId: Int, val title: String
+	val slug: String, val tmdbId: Int, val title: String, val releaseDate: Instant?
 ) {
-	constructor(tmdbId: Int, title: String) : this(slug = title.toSlug(), tmdbId, title)
-
-	companion object
+	constructor(tmdbId: Int, title: String, releaseDate: Instant? = null) : this(
+		slug = title.toSlug(), tmdbId, title, releaseDate
+	)
 }
-
-val Production.Companion.sampleDataSet: Array<Production>
-	get() = arrayOf(Production(1726, "Iron Man"), Production(10138, "Iron Man 2"), Production(68721, "Iron Man 3"))
