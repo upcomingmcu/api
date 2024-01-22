bce23bf5b700f86d27b058162820465bae3957a5
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-22 09:23:08 -0700

    Update error handling

--- src/main/kotlin/app/umcu/api/plugins/Routing.kt
+++ src/main/kotlin/app/umcu/api/plugins/Routing.kt
@@ -1,9 +1,11 @@
 package app.umcu.api.plugins
 
+import app.umcu.api.models.ErrorResponse
 import app.umcu.api.routes.productionsRoute
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
@@ -13,14 +15,16 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
 	install(StatusPages) {
 		exception<Throwable> { call, cause ->
-			call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+			val code = HttpStatusCode.InternalServerError
+			call.respond(code, ErrorResponse(code, cause.localizedMessage))
 		}
-		status(HttpStatusCode.NotFound) { call, code ->
-			call.respondText(text = code.toString(), status = code)
+		exception<NotFoundException> { call, cause ->
+			val code = HttpStatusCode.NotFound
+			call.respond(code, ErrorResponse(code, cause.localizedMessage))
 		}
-		status(HttpStatusCode.TooManyRequests) { call, code ->
-			call.respondText(text = code.toString(), status = code)
-		}
+
+		status(HttpStatusCode.NotFound) { call, code -> call.respond(code, ErrorResponse(code)) }
+		status(HttpStatusCode.TooManyRequests) { call, code -> call.respond(code, ErrorResponse(code)) }
 	}
 	install(Resources)
 
