318aebb457a3558f518f4aa7677bf77903b67724
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-24 16:50:54 -0700

    Define TMDB API `/tv` models

---
+++ src/main/kotlin/app/umcu/api/remote/models/Series.kt
@@ -0,0 +1,27 @@
+package app.umcu.api.remote.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+object Series {
+	@Serializable
+	data class Details(
+		@SerialName("first_air_date") val firstAirDate: String,
+		@SerialName("id") val seriesId: Int,
+		val name: String,
+		val overview: String,
+		@SerialName("poster_path") val posterPath: String,
+		val seasons: List<Season>
+	)
+
+	@Serializable
+	data class Season(
+		@SerialName("air_date") val airDate: String,
+		@SerialName("episode_count") val episodeCount: Int,
+		@SerialName("id") val seasonId: Int,
+		val name: String,
+		val overview: String,
+		@SerialName("poster_path") val posterPath: String,
+		@SerialName("season_number") val seasonNumber: Int
+	)
+}
