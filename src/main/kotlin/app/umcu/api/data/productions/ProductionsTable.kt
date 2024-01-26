f509a9350ca0c2dcf30e6243af245ee1c39a9a45
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-25 18:05:57 -0700

    Document each column in the productions table

--- src/main/kotlin/app/umcu/api/data/productions/ProductionsTable.kt
+++ src/main/kotlin/app/umcu/api/data/productions/ProductionsTable.kt
@@ -7,8 +7,25 @@ import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 object ProductionsTable : IntIdTable() {
+	/**
+	 * The human-friendly unique identifier of the production.
+	 * Determined by the production's title. For example: "Avengers: Infinity War" would be "avengers-infinity-war".
+	 */
 	val slug: Column<String> = text("slug").uniqueIndex()
+
+	/**
+	 * The identifier of the production on The Movie Database.
+	 * Unique to TMDB but not necessarily for this application (for example, series with multiple seasons will share this value).
+	 */
 	val tmdbId: Column<Int> = integer("tmdbId")
+
+	/**
+	 * The title of the production.
+	 */
 	val title: Column<String> = text("title")
+
+	/**
+	 * The release date of the production. In the format of an ISO 8601 date and time.
+	 */
 	val releaseDate: Column<Instant?> = timestamp("releaseDate").nullable()
 }
