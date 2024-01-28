4469c17e432d9d1d8e19d7e4b0471be18f2e0793
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-01-28 09:39:36 -0700

    Rename to configureProductionsRoute

--- src/main/kotlin/app/umcu/api/routes/Productions.kt
+++ src/main/kotlin/app/umcu/api/routes/Productions.kt
@@ -10,117 +10,114 @@ import io.bkbn.kompendium.oas.payload.Parameter
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
-import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.productionsRoute() {
-	rateLimit {
-		route("/v1/productions") {
-			// allProductions
-			route("") {
-				allProductionsDoc()
+fun Route.configureProductionsRoute() {
+    route("/v1/productions") {
+        // allProductions
+        route("") {
+            allProductionsDoc()
 
-				get {
-					call.respond(Response(ProductionsDAOFacadeImpl().allProductions()))
-				}
-			}
+            get {
+                call.respond(Response(ProductionsDAOFacadeImpl().allProductions()))
+            }
+        }
 
-			// productionBySlug
-			route("/{slug}") {
-				productionBySlugDoc()
+        // productionBySlug
+        route("/{slug}") {
+            productionBySlugDoc()
 
-				get {
-					val slug = call.parameters["slug"] ?: throw BadRequestException("Slug parameter is required.")
-					val data = ProductionsDAOFacadeImpl().productionBySlug(slug) ?: throw NotFoundException()
-					call.respond(Response(data))
-				}
-			}
+            get {
+                val slug = call.parameters["slug"] ?: throw BadRequestException("Slug parameter is required.")
+                val data = ProductionsDAOFacadeImpl().productionBySlug(slug) ?: throw NotFoundException()
+                call.respond(Response(data))
+            }
+        }
 
-			// nextProduction
-			route("/next") {
-				nextProductionDoc()
+        // nextProduction
+        route("/next") {
+            nextProductionDoc()
 
-				get {
-					throw NotFoundException()
-				}
-			}
-		}
-	}
+            get {
+                throw NotFoundException()
+            }
+        }
+    }
 }
 
 private fun Route.allProductionsDoc() {
-	install(NotarizedRoute()) {
-		tags = setOf(RoutingTags.PRODUCTIONS.toString())
-		get = GetInfo.builder {
-			summary("Get all productions.")
-			description("Get a list of all previously released, upcoming, and to be announced productions.")
-			operationId("allProductions")
-			response {
-				responseType<Response<Production>>()
-				responseCode(HttpStatusCode.OK)
-				description("All productions.")
-			}
-		}
-	}
+    install(NotarizedRoute()) {
+        tags = setOf(RoutingTags.PRODUCTIONS.toString())
+        get = GetInfo.builder {
+            summary("Get all productions.")
+            description("Get a list of all previously released, upcoming, and to be announced productions.")
+            operationId("allProductions")
+            response {
+                responseType<Response<Production>>()
+                responseCode(HttpStatusCode.OK)
+                description("All productions.")
+            }
+        }
+    }
 }
 
 private fun Route.productionBySlugDoc() {
-	install(NotarizedRoute()) {
-		tags = setOf(RoutingTags.PRODUCTIONS.toString())
-		get = GetInfo.builder {
-			summary("Get a specific production by its slug.")
-			description("Get a single production in the database by its unique slug identifier.")
-			operationId("productionBySlug")
-			parameters = listOf(
-				Parameter(
-					name = "slug",
-					`in` = Parameter.Location.path,
-					schema = TypeDefinition.STRING,
-					description = "The slug of the production you want to get.",
-					allowEmptyValue = false,
-				)
-			)
-			response {
-				responseType<Response<Production>>()
-				responseCode(HttpStatusCode.OK)
-				description("The production that matches the slug.")
-			}
-			canRespond {
-				responseType<NotFoundException>()
-				responseCode(HttpStatusCode.NotFound)
-				description("No production by that slug could be found.")
-			}
-		}
-	}
+    install(NotarizedRoute()) {
+        tags = setOf(RoutingTags.PRODUCTIONS.toString())
+        get = GetInfo.builder {
+            summary("Get a specific production by its slug.")
+            description("Get a single production in the database by its unique slug identifier.")
+            operationId("productionBySlug")
+            parameters = listOf(
+                Parameter(
+                    name = "slug",
+                    `in` = Parameter.Location.path,
+                    schema = TypeDefinition.STRING,
+                    description = "The slug of the production you want to get.",
+                    allowEmptyValue = false,
+                )
+            )
+            response {
+                responseType<Response<Production>>()
+                responseCode(HttpStatusCode.OK)
+                description("The production that matches the slug.")
+            }
+            canRespond {
+                responseType<NotFoundException>()
+                responseCode(HttpStatusCode.NotFound)
+                description("No production by that slug could be found.")
+            }
+        }
+    }
 }
 
 private fun Route.nextProductionDoc() {
-	install(NotarizedRoute()) {
-		tags = setOf(RoutingTags.PRODUCTIONS.toString())
-		get = GetInfo.builder {
-			summary("Get the next production to be released.")
-			description("Get the next production to be released following the current, or provided, date.")
-			operationId("nextProduction")
-			parameters = listOf(
-				Parameter(
-					name = "date",
-					`in` = Parameter.Location.query,
-					schema = TypeDefinition.STRING,
-					description = "The date to use when getting the next production.",
-					allowEmptyValue = true,
-				)
-			)
-			response {
-				responseType<Response<Production>>()
-				responseCode(HttpStatusCode.OK)
-				description("The next production to be released.")
-			}
-			canRespond {
-				responseType<NotFoundException>()
-				responseCode(HttpStatusCode.NotFound)
-				description("No production is upcoming.")
-			}
-		}
-	}
+    install(NotarizedRoute()) {
+        tags = setOf(RoutingTags.PRODUCTIONS.toString())
+        get = GetInfo.builder {
+            summary("Get the next production to be released.")
+            description("Get the next production to be released following the current, or provided, date.")
+            operationId("nextProduction")
+            parameters = listOf(
+                Parameter(
+                    name = "date",
+                    `in` = Parameter.Location.query,
+                    schema = TypeDefinition.STRING,
+                    description = "The date to use when getting the next production.",
+                    allowEmptyValue = true,
+                )
+            )
+            response {
+                responseType<Response<Production>>()
+                responseCode(HttpStatusCode.OK)
+                description("The next production to be released.")
+            }
+            canRespond {
+                responseType<NotFoundException>()
+                responseCode(HttpStatusCode.NotFound)
+                description("No production is upcoming.")
+            }
+        }
+    }
 }
