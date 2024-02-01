4fbedf8f8f3f28e0b548268d91f73c237b7dcf9e
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-02-01 16:58:17 -0700

    Set properties as nullable

--- src/main/kotlin/app/umcu/api/remote/models/Series.kt
+++ src/main/kotlin/app/umcu/api/remote/models/Series.kt
@@ -6,22 +6,22 @@ import kotlinx.serialization.Serializable
 object Series {
 	@Serializable
 	data class Details(
-		@SerialName("first_air_date") val firstAirDate: String,
+		@SerialName("first_air_date") val firstAirDate: String?,
 		@SerialName("id") val seriesId: Int,
 		val name: String,
-		val overview: String,
-		@SerialName("poster_path") val posterPath: String,
+		val overview: String?,
+		@SerialName("poster_path") val posterPath: String?,
 		val seasons: List<Season>
 	)
 
 	@Serializable
 	data class Season(
-		@SerialName("air_date") val airDate: String,
-		@SerialName("episode_count") val episodeCount: Int,
+		@SerialName("air_date") val airDate: String?,
+		@SerialName("episode_count") val episodeCount: Int?,
 		@SerialName("id") val seasonId: Int,
 		val name: String,
-		val overview: String,
-		@SerialName("poster_path") val posterPath: String,
+		val overview: String?,
+		@SerialName("poster_path") val posterPath: String?,
 		@SerialName("season_number") val seasonNumber: Int
 	)
 }
