06232b5d24ce6aa9d8ec59df476d83337726bc86
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-02-05 19:06:36 -0700

    Update ProductionsTable definitions

--- src/main/kotlin/app/umcu/api/database/tables/ProductionsTable.kt
+++ src/main/kotlin/app/umcu/api/database/tables/ProductionsTable.kt
@@ -1,31 +1,29 @@
 package app.umcu.api.database.tables
 
-import kotlinx.datetime.Instant
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
 @Suppress("MemberVisibilityCanBePrivate", "unused")
-object ProductionsTable : IntIdTable() {
+object ProductionsTable : IntIdTable("productions") {
 	/**
 	 * The human-friendly unique identifier of the production.
 	 * Determined by the production's title. For example: "Avengers: Infinity War" would be "avengers-infinity-war".
 	 */
-	val slug: Column<String> = text("slug").uniqueIndex()
+	val slug = varchar("slug", 255).uniqueIndex()
 
 	/**
 	 * The identifier of the production on The Movie Database.
 	 * Unique to TMDB but not necessarily for this application (for example, series with multiple seasons will share this value).
 	 */
-	val tmdbId: Column<Int> = integer("tmdbId")
+	val tmdbId = integer("tmdb_id")
 
 	/**
 	 * The title of the production.
 	 */
-	val title: Column<String> = text("title")
+	val title = varchar("title", 255)
 
 	/**
 	 * The release date of the production. In the format of an ISO 8601 date and time.
 	 */
-	val releaseDate: Column<Instant?> = timestamp("releaseDate").nullable()
+	val releaseDate = timestamp("release_date").nullable()
 }
