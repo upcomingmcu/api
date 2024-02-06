7010cf076f9598ab29807c6adcee7d6fec82beaa
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-02-06 07:01:53 -0700

    Remove function ProductionsServiceImpl::daoToProduction

--- src/main/kotlin/app/umcu/api/features/productions/service/ProductionsServiceImpl.kt
+++ src/main/kotlin/app/umcu/api/features/productions/service/ProductionsServiceImpl.kt
@@ -4,20 +4,17 @@ import app.umcu.api.database.DatabaseSingleton
 import app.umcu.api.database.dao.ProductionDao
 import app.umcu.api.database.tables.ProductionsTable
 import app.umcu.api.features.productions.models.Production
+import app.umcu.api.features.productions.models.Production.Companion.toProduction
 import org.koin.java.KoinJavaComponent.inject
 
 class ProductionsServiceImpl : ProductionsService {
 	private val databaseSingleton: DatabaseSingleton by inject(DatabaseSingleton::class.java)
 
-	private fun daoToProduction(dao: ProductionDao) = Production(
-		slug = dao.slug, tmdbId = dao.tmdbId, title = dao.title, releaseDate = dao.releaseDate
-	)
-
 	override suspend fun allProductions(): List<Production> = databaseSingleton.query {
-		ProductionDao.all().map(::daoToProduction)
+		ProductionDao.all().map(::toProduction)
 	}
 
 	override suspend fun productionBySlug(slug: String): Production? = databaseSingleton.query {
-		ProductionDao.find { ProductionsTable.slug eq slug }.map(::daoToProduction).firstOrNull()
+		ProductionDao.find { ProductionsTable.slug eq slug }.map(::toProduction).firstOrNull()
 	}
 }
